a0e0a8c2ba27cf5f62d907d1f75bc73293be7033
Kevin Rutherford <kevin@rutherford-software.com>
2013-01-16 16:57:50 +0000

    First basic gem files

---
+++ event_bus.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'event_bus'
+  s.version     = '0.0.1'
+  s.date        = '2013-01-16'
+  s.summary     = 'A simple pubsub event bus'
+  s.description = 'A simple pubsub event bus'
+  s.authors     = ['Kevin Rutherford']
+  s.email       = 'kevin@rutherford-software.com'
+  s.files       = ['lib/event_bus.rb']
+  s.homepage    = 'http://rubygems.org/gems/event_bus'
+end
+
---
+++ lib/event_bus.rb
@@ -0,0 +1,43 @@
+class EventBus
+  include Singleton
+
+  def self.announce(event_name, details)
+    instance.announce(event_name, details)
+  end
+
+  def self.clear
+    instance.clear
+  end
+
+  def self.listen_for(pattern, listener, method_name)
+    instance.register(pattern, listener, method_name)
+  end
+
+  def self.register(listener)
+    listener.events_map.each do |pattern, method_name|
+      instance.register(pattern, listener, method_name)
+    end
+  end
+
+  Registration = Struct.new(:pattern, :listener, :method_name) do
+    def respond(event_name, details)
+      listener.send(method_name, details) if pattern === event_name
+    end
+  end
+
+  def announce(event_name, details)
+    @listeners.each do |listener|
+      listener.respond(event_name, details)
+    end
+  end
+
+  def clear
+    @listeners = []
+  end
+
+  def register(pattern, listener, method_name)
+    @listeners << Registration.new(pattern, listener, method_name)
+  end
+
+end
+
