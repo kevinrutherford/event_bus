8c6457e1c150b9aac60958298cf975de808c6305
Kevin Rutherford <kevin@rutherford-software.com>
2013-01-16 20:43:44 +0000

    Added the first simple test

--- spec/lib/event_bus_spec.rb
+++ spec/lib/event_bus_spec.rb
@@ -2,5 +2,17 @@ require 'spec_helper'
 
 describe EventBus do
 
+  describe '.announce' do
+
+    context 'with no payload' do
+      it 'sends an event to a listener' do
+        listener = double(:listener)
+        listener.should_receive(:fred)
+        EventBus.listen_for('aaa', listener, :fred)
+        EventBus.announce('aaa', {})
+      end
+    end
+  end
+
 end
 
