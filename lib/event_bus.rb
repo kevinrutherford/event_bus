dc17b0fc64f5e751cc8bfe039833cf8f1596eda3
Kevin Rutherford <kevin@rutherford-software.com>
2013-01-18 08:59:44 +0000

    Moved the internal workings out of the public API

--- lib/event_bus.rb
+++ lib/event_bus.rb
@@ -1,48 +1,58 @@
 require 'singleton'
 
 class EventBus
-  include Singleton
 
   def self.announce(event_name, details)
-    instance.announce(event_name, details)
+    Registrations.instance.announce(event_name, details)
+    self
   end
 
   def self.clear
-    instance.clear
+    Registrations.instance.clear
+    self
   end
 
   def self.listen_for(pattern, listener, method_name)
-    instance.register(pattern, listener, method_name)
+    Registrations.instance.register(pattern, listener, method_name)
+    self
   end
 
   def self.register(listener)
     listener.events_map.each do |pattern, method_name|
-      instance.register(pattern, listener, method_name)
+      Registrations.instance.register(pattern, listener, method_name)
     end
+    self
   end
 
-  Registration = Struct.new(:pattern, :listener, :method_name) do
-    def respond(event_name, details)
-      listener.send(method_name, details) if pattern === event_name
+  private
+
+  class Registrations
+    include Singleton
+
+    Registration = Struct.new(:pattern, :listener, :method_name) do
+      def respond(event_name, details)
+        listener.send(method_name, details) if pattern === event_name
+      end
     end
-  end
 
-  def initialize
-    clear
-  end
-
-  def announce(event_name, details)
-    @listeners.each do |listener|
-      listener.respond(event_name, details)
+    def initialize
+      clear
     end
-  end
 
-  def clear
-    @listeners = []
-  end
+    def announce(event_name, details)
+      @listeners.each do |listener|
+        listener.respond(event_name, details)
+      end
+    end
 
-  def register(pattern, listener, method_name)
-    @listeners << Registration.new(pattern, listener, method_name)
+    def clear
+      @listeners = []
+    end
+
+    def register(pattern, listener, method_name)
+      @listeners << Registration.new(pattern, listener, method_name)
+    end
+
   end
 
 end
--- spec/lib/event_bus_spec.rb
+++ spec/lib/event_bus_spec.rb
@@ -3,17 +3,18 @@ require 'spec_helper'
 describe EventBus do
   let(:listener) { double(:listener) }
   let(:event_name) { 'aa123bb' }
-  let(:receiver) { :receiving_method }
+  let(:receiving_method) { :receiving_method_name }
 
   before do
-    EventBus.listen_for(event_name, listener, receiver)
+    EventBus.clear
+    EventBus.listen_for(event_name, listener, receiving_method)
   end
 
   describe '.announce' do
 
     context 'with no payload' do
       it 'sends an event to a listener' do
-        listener.should_receive(receiver)
+        listener.should_receive(receiving_method)
         EventBus.announce(event_name, {})
       end
     end
@@ -22,8 +23,12 @@ describe EventBus do
   describe '.clear' do
     it 'sends no event to previous registrants' do
       EventBus.clear
-      listener.should_not_receive(receiver)
+      listener.should_not_receive(receiving_method)
       EventBus.announce(event_name, {})
+    end
+
+    it 'returns itself, to facilitate cascades' do
+      EventBus.clear.should == EventBus
     end
   end
 
