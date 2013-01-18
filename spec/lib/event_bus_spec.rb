37d600d0a5ce3b8aac5459c3f52444c28faa4713
Kevin Rutherford <kevin@rutherford-software.com>
2013-01-18 08:41:09 +0000

    Improve test coverage

--- spec/lib/event_bus_spec.rb
+++ spec/lib/event_bus_spec.rb
@@ -1,16 +1,29 @@
 require 'spec_helper'
 
 describe EventBus do
+  let(:listener) { double(:listener) }
+  let(:event_name) { 'aa123bb' }
+  let(:receiver) { :receiving_method }
+
+  before do
+    EventBus.listen_for(event_name, listener, receiver)
+  end
 
   describe '.announce' do
 
     context 'with no payload' do
       it 'sends an event to a listener' do
-        listener = double(:listener)
-        listener.should_receive(:fred)
-        EventBus.listen_for('aaa', listener, :fred)
-        EventBus.announce('aaa', {})
+        listener.should_receive(receiver)
+        EventBus.announce(event_name, {})
       end
+    end
+  end
+
+  describe '.clear' do
+    it 'sends no event to previous registrants' do
+      EventBus.clear
+      listener.should_not_receive(receiver)
+      EventBus.announce(event_name, {})
     end
   end
 
