a060aa0d873f2fb6cca3f5456d7210a4f7cc59d2
Kevin Rutherford <kevin@rutherford-software.com>
2013-01-21 16:52:33 +0000

    Added some initial Yardoc for rubydoc.info

--- lib/event_bus.rb
+++ lib/event_bus.rb
@@ -2,21 +2,44 @@ require 'singleton'
 
 class EventBus
 
+  #
+  # Announce an event to any waiting listeners.
+  #
+  # @param event_name [String, Symbol] the name of your event
+  # @param details [Hash] the information you want to pass to the listeners
+  # @return the EventBus, ready to be called again.
+  #
   def self.announce(event_name, details)
     Registrations.instance.announce(event_name, details)
     self
   end
 
-  def self.clear
-    Registrations.instance.clear
-    self
-  end
-
+  #
+  # Register a single listener.
+  #
+  # @param pattern [String, Regex] listen for any events whose name matches this pattern
+  # @param listener the object to be notified when a matching event occurs
+  # @param method_name [Symbol] the method to be called on listener when a matching event occurs
+  # @return the EventBus, ready to be called again.
+  #
   def self.listen_for(pattern, listener, method_name)
     Registrations.instance.register(pattern, listener, method_name)
     self
   end
 
+  #
+  # Delete all current listener registrations
+  #
+  # @return the EventBus, ready to be called again.
+  #
+  def self.clear
+    Registrations.instance.clear
+    self
+  end
+
+  #
+  # (experimental)
+  #
   def self.register(listener)
     listener.events_map.each do |pattern, method_name|
       Registrations.instance.register(pattern, listener, method_name)
