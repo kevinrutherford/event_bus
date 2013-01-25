defc503cd8f3939f858c6452a6d454c6841bec0c
Kevin Rutherford <kevin@rutherford-software.com>
2013-01-25 15:42:14 +0000

    Added some aliases for the main EventBus methods

--- lib/event_bus.rb
+++ lib/event_bus.rb
@@ -2,58 +2,67 @@ require 'singleton'
 
 class EventBus
 
-  #
-  # Announce an event to any waiting listeners.
-  #
-  # The +event_name+ is added to the +details+ hash (with the key +:event_name+)
-  # before the event is passed on to listeners.
-  #
-  # @param event_name [String, Symbol] the name of your event
-  # @param details [Hash] the information you want to pass to the listeners
-  # @return the EventBus, ready to be called again.
-  #
-  def self.announce(event_name, details)
-    registrations.announce(event_name, details)
-    self
-  end
+  class << self
 
-  #
-  # Register a single listener.
-  #
-  # @param pattern [String, Regex] listen for any events whose name matches this pattern
-  # @param listener the object to be notified when a matching event occurs
-  # @param method_name [Symbol] the method to be called on listener when a matching event occurs
-  # @return the EventBus, ready to be called again.
-  #
-  def self.listen_for(pattern, listener, method_name)
-    registrations.register(pattern, listener, method_name)
-    self
-  end
-
-  #
-  # Delete all current listener registrations
-  #
-  # @return the EventBus, ready to be called again.
-  #
-  def self.clear
-    registrations.clear
-    self
-  end
-
-  #
-  # (experimental)
-  #
-  def self.register(listener)
-    listener.events_map.each do |pattern, method_name|
-      registrations.register(pattern, listener, method_name)
+    #
+    # Announce an event to any waiting listeners.
+    #
+    # The +event_name+ is added to the +details+ hash (with the key +:event_name+)
+    # before the event is passed on to listeners.
+    #
+    # @param event_name [String, Symbol] the name of your event
+    # @param details [Hash] the information you want to pass to the listeners
+    # @return the EventBus, ready to be called again.
+    #
+    def publish(event_name, details)
+      registrations.announce(event_name, details)
+      self
     end
-    self
-  end
 
-  private
+    alias :announce :publish
+    alias :broadcast :publish
 
-  def self.registrations
-    Registrations.instance
+    #
+    # Register a single listener.
+    #
+    # @param pattern [String, Regex] listen for any events whose name matches this pattern
+    # @param listener the object to be notified when a matching event occurs
+    # @param method_name [Symbol] the method to be called on listener when a matching event occurs
+    # @return the EventBus, ready to be called again.
+    #
+    def subscribe(pattern, listener, method_name)
+      registrations.register(pattern, listener, method_name)
+      self
+    end
+
+    alias :listen_for :subscribe
+
+    #
+    # Delete all current listener registrations
+    #
+    # @return the EventBus, ready to be called again.
+    #
+    def clear
+      registrations.clear
+      self
+    end
+
+    #
+    # (experimental)
+    #
+    def register(listener)
+      listener.events_map.each do |pattern, method_name|
+        registrations.register(pattern, listener, method_name)
+      end
+      self
+    end
+
+    private
+
+    def registrations
+      Registrations.instance
+    end
+
   end
 
   class Registrations
--- spec/lib/event_bus_spec.rb
+++ spec/lib/event_bus_spec.rb
@@ -9,58 +9,58 @@ describe EventBus do
     EventBus.clear
   end
 
-  describe '.announce' do
+  describe '.publish' do
 
     it 'returns itself, to facilitate cascades' do
-      EventBus.announce(event_name, {}).should == EventBus
+      EventBus.publish(event_name, {}).should == EventBus
     end
 
     it 'passes the event name in the details hash' do
-      EventBus.listen_for(event_name, listener, receiving_method)
+      EventBus.subscribe(event_name, listener, receiving_method)
       listener.should_receive(receiving_method).with({:event_name => event_name})
-      EventBus.announce(event_name, {}).should == EventBus
+      EventBus.publish(event_name, {}).should == EventBus
     end
 
     context 'when the listener is specific about the event name' do
       it 'sends the event to the listener' do
-        EventBus.listen_for(event_name, listener, receiving_method)
+        EventBus.subscribe(event_name, listener, receiving_method)
         listener.should_receive(receiving_method).with({:a => 1, :b => 2, :event_name => event_name})
-        EventBus.announce(event_name, {:a => 1, :b => 2})
+        EventBus.publish(event_name, {:a => 1, :b => 2})
       end
     end
 
     context 'when the listener uses a regex that matches' do
       it 'sends the event to the listener' do
-        EventBus.listen_for(/123b/, listener, receiving_method)
+        EventBus.subscribe(/123b/, listener, receiving_method)
         listener.should_receive(receiving_method).with({:a => 1, :b => 2, :event_name => event_name})
-        EventBus.announce(event_name, {:a => 1, :b => 2})
+        EventBus.publish(event_name, {:a => 1, :b => 2})
       end
     end
 
     context 'when the listener listens for a different event' do
       it 'does not send the event to the listener' do
-        EventBus.listen_for('blah', listener, receiving_method)
+        EventBus.subscribe('blah', listener, receiving_method)
         listener.should_not_receive(receiving_method)
-        EventBus.announce(event_name, {:a => 1, :b => 2, :event_name => event_name})
+        EventBus.publish(event_name, {:a => 1, :b => 2, :event_name => event_name})
       end
     end
 
     context 'when the listener listens for a non-matching regex' do
       it 'does not send the event to the listener' do
-        EventBus.listen_for(/123a/, listener, receiving_method)
+        EventBus.subscribe(/123a/, listener, receiving_method)
         listener.should_not_receive(receiving_method)
-        EventBus.announce(event_name, {:a => 1, :b => 2, :event_name => event_name})
+        EventBus.publish(event_name, {:a => 1, :b => 2, :event_name => event_name})
       end
     end
 
   end
 
   describe '.clear' do
-    it 'sends no event to previous registrants' do
-      EventBus.listen_for(event_name, listener, receiving_method)
+    it 'removes all previous registrants' do
+      EventBus.subscribe(event_name, listener, receiving_method)
       EventBus.clear
       listener.should_not_receive(receiving_method)
-      EventBus.announce(event_name, {})
+      EventBus.publish(event_name, {})
     end
 
     it 'returns itself, to facilitate cascades' do
