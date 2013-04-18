78fbd818d2c6487cfc959e88c34a3b0d7d0e8149
Kevin Rutherford <kevin@rutherford-software.com>
2013-04-18 07:49:41 +0100

    Split the private class into a separate sourcefile

---
+++ lib/event_bus/registrations.rb
@@ -0,0 +1,56 @@
+require 'singleton'
+
+class EventBus
+
+  class Registrations
+    include Singleton
+
+    def initialize
+      clear
+    end
+
+    def announce(event_name, payload)
+      full_payload = {event_name: event_name}.merge(payload)
+      @listeners.each do |listener|
+        listener.respond(event_name, full_payload)
+      end
+    end
+
+    def clear
+      @listeners = []
+    end
+
+    def add(pattern, listener, method_name, &blk)
+      if listener
+        add_method(pattern, listener, method_name)
+      else
+        add_block(pattern, blk)
+      end
+    end
+
+    def add_method(pattern, listener, method_name)
+      @listeners << Registration.new(pattern, listener, method_name)
+    end
+
+    def add_block(pattern, &blk)
+      @listeners << BlockRegistration.new(pattern, blk)
+    end
+
+    private
+
+    Registration = Struct.new(:pattern, :listener, :method_name) do
+      def respond(event_name, payload)
+        listener.send(method_name, payload) if pattern === event_name
+      end
+    end
+
+    BlockRegistration = Struct.new(:pattern, :block) do
+      def respond(event_name, payload)
+        block.call(payload) if pattern === event_name
+      end
+    end
+
+  end
+
+end
+
