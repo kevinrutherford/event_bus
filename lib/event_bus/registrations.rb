3c9734a01743f15501ffa9375b5498269040e668
Kevin Rutherford <kevin@rutherford-software.com>
2013-04-23 08:01:09 +0100

    Fixed the one problem found by mutant

--- lib/event_bus/registrations.rb
+++ lib/event_bus/registrations.rb
@@ -7,30 +7,30 @@ class EventBus
   class Registrations
     include Singleton
 
-    def initialize
-      clear
-    end
-
     def announce(event_name, payload)
       full_payload = {event_name: event_name}.merge(payload)
-      @listeners.each do |listener|
+      listeners.each do |listener|
         listener.respond(event_name, full_payload)
       end
     end
 
     def clear
-      @listeners = []
+      listeners.clear
     end
 
     def add_method(pattern, listener, method_name)
-      @listeners << Registration.new(pattern, listener, method_name)
+      listeners << Registration.new(pattern, listener, method_name)
     end
 
     def add_block(pattern, &blk)
-      @listeners << BlockRegistration.new(pattern, blk)
+      listeners << BlockRegistration.new(pattern, blk)
     end
 
     private
+
+    def listeners
+      @listeners ||= []
+    end
 
     Registration = Struct.new(:pattern, :listener, :method_name) do
       def respond(event_name, payload)
