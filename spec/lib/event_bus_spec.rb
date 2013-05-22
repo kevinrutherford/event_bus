24f2562665c2ddcdd3db3dc6b5641cd1bc06aa71
Kevin Rutherford <kevin@rutherford-software.com>
2013-05-22 19:09:56 +0100

    Made the receiving method name explicit in the tests, for clarity

--- spec/lib/event_bus_spec.rb
+++ spec/lib/event_bus_spec.rb
@@ -3,26 +3,25 @@ require 'spec_helper'
 describe EventBus do
   let(:listener) { double(:listener) }
   let(:event_name) { 'aa123bb' }
-  let(:receiving_method) { :receiving_method_name }
 
   before do
     EventBus.clear
-    listener.stub(receiving_method) { }
+    listener.stub(:handler) { }
   end
 
   describe 'publishing' do
 
     it 'accepts a string for the event name' do
-      EventBus.subscribe(/#{event_name}/, listener, receiving_method)
+      EventBus.subscribe(/#{event_name}/, listener, :handler)
       EventBus.publish(event_name)
-      listener.should have_received(receiving_method).with(event_name: event_name)
+      listener.should have_received(:handler).with(event_name: event_name)
     end
 
     it 'accepts a symbol for the event name' do
       event_sym = :abc_123
-      EventBus.subscribe(/#{event_sym}/, listener, receiving_method)
+      EventBus.subscribe(/#{event_sym}/, listener, :handler)
       EventBus.publish(event_sym)
-      listener.should have_received(receiving_method).with(event_name: event_sym)
+      listener.should have_received(:handler).with(event_name: event_sym)
     end
 
     it 'rejects any other type as the event name' do
@@ -34,15 +33,15 @@ describe EventBus do
     end
 
     it 'adds the event name to the payload' do
-      EventBus.subscribe(event_name, listener, receiving_method)
+      EventBus.subscribe(event_name, listener, :handler)
       EventBus.publish(event_name, a: 56)
-      listener.should have_received(receiving_method).with(event_name: event_name, a: 56)
+      listener.should have_received(:handler).with(event_name: event_name, a: 56)
     end
 
     it 'allows the payload to be omitted' do
-      EventBus.subscribe(event_name, listener, receiving_method)
+      EventBus.subscribe(event_name, listener, :handler)
       EventBus.publish(event_name)
-      listener.should have_received(receiving_method).with(event_name: event_name)
+      listener.should have_received(:handler).with(event_name: event_name)
     end
 
   end
@@ -52,20 +51,20 @@ describe EventBus do
     let(:error_handler) { double(:error_handler) }
 
     before do
-      erroring_listener.stub(receiving_method) { raise RuntimeError.new }
+      erroring_listener.stub(:handler) { raise RuntimeError.new }
       error_handler.stub(:handle_error) {}
     end
 
     it 'sends the event to the second listener when the first errors' do
-      EventBus.subscribe(event_name, erroring_listener, receiving_method)
-      EventBus.subscribe(event_name, listener, receiving_method)
+      EventBus.subscribe(event_name, erroring_listener, :handler)
+      EventBus.subscribe(event_name, listener, :handler)
 
       EventBus.publish(event_name)
-      listener.should have_received(receiving_method).with(event_name: event_name)
+      listener.should have_received(:handler).with(event_name: event_name)
     end
 
     it 'calls the error handler on an error when the listener is an object' do
-      EventBus.subscribe(event_name, erroring_listener, receiving_method)
+      EventBus.subscribe(event_name, erroring_listener, :handler)
       EventBus.on_error do |listener, full_payload|
         error_handler.handle_error listener, full_payload
       end
@@ -93,55 +92,55 @@ describe EventBus do
   describe 'subscribing' do
 
     it 'returns itself, to facilitate cascades' do
-      EventBus.subscribe(event_name, listener, receiving_method).should == EventBus
+      EventBus.subscribe(event_name, listener, :handler).should == EventBus
     end
 
     context 'with a regex pattern' do
       it 'sends the event to a matching listener' do
-        EventBus.subscribe(/123b/, listener, receiving_method)
+        EventBus.subscribe(/123b/, listener, :handler)
         EventBus.publish(event_name, a: 1, b: 2)
-        listener.should have_received(receiving_method).with(a: 1, b: 2, event_name: event_name)
+        listener.should have_received(:handler).with(a: 1, b: 2, event_name: event_name)
       end
 
       it 'does not send the event to non-matching listeners' do
-        EventBus.subscribe(/123a/, listener, receiving_method)
+        EventBus.subscribe(/123a/, listener, :handler)
         EventBus.publish(event_name, a: 1, b: 2, event_name: event_name)
-        listener.should_not have_received(receiving_method)
+        listener.should_not have_received(:handler)
       end
     end
 
     context 'with a string pattern' do
       it 'sends the event to a matching listener' do
-        EventBus.subscribe(event_name, listener, receiving_method)
+        EventBus.subscribe(event_name, listener, :handler)
         EventBus.publish(event_name, a: 1, b: 2)
-        listener.should have_received(receiving_method).with(a: 1, b: 2, event_name: event_name)
+        listener.should have_received(:handler).with(a: 1, b: 2, event_name: event_name)
       end
 
       it 'does not send the event to non-matching listeners' do
-        EventBus.subscribe('blah', listener, receiving_method)
+        EventBus.subscribe('blah', listener, :handler)
         EventBus.publish(event_name, a: 1, b: 2, event_name: event_name)
-        listener.should_not have_received(receiving_method)
+        listener.should_not have_received(:handler)
       end
     end
 
     context 'with a symbol pattern' do
       it 'sends the event to a matching listener' do
         event_name = :abc_123
-        EventBus.subscribe(event_name, listener, receiving_method)
+        EventBus.subscribe(event_name, listener, :handler)
         EventBus.publish(event_name, a: 1, b: 2)
-        listener.should have_received(receiving_method).with(a: 1, b: 2, event_name: event_name)
+        listener.should have_received(:handler).with(a: 1, b: 2, event_name: event_name)
       end
 
       it 'does not send the event to non-matching listeners' do
-        EventBus.subscribe(:blah, listener, receiving_method)
+        EventBus.subscribe(:blah, listener, :handler)
         EventBus.publish(event_name, a: 1, b: 2, event_name: event_name)
-        listener.should_not have_received(receiving_method)
+        listener.should_not have_received(:handler)
       end
     end
 
     context 'with a listener method' do
       it 'will not accept a block too' do
-        expect { EventBus.subscribe('blah', listener, receiving_method) {|info| }}.to raise_error(ArgumentError)
+        expect { EventBus.subscribe('blah', listener, :handler) {|info| }}.to raise_error(ArgumentError)
       end
 
       it 'expects a method name' do
@@ -176,20 +175,20 @@ describe EventBus do
 
       it 'calls a listener method whose name matches the event name' do
         EventBus.subscribe(listener)
-        EventBus.publish(receiving_method.to_s, a: 2, b: 3)
-        listener.should have_received(receiving_method).with(a: 2, b: 3, event_name: receiving_method.to_s)
+        EventBus.publish(:handler.to_s, a: 2, b: 3)
+        listener.should have_received(:handler).with(a: 2, b: 3, event_name: :handler.to_s)
       end
 
       it 'calls a listener method with symbol whose name matches the event name' do
         EventBus.subscribe(listener)
-        EventBus.publish(receiving_method, a: 2, b: 3)
-        listener.should have_received(receiving_method).with(a: 2, b: 3, event_name: receiving_method)
+        EventBus.publish(:handler, a: 2, b: 3)
+        listener.should have_received(:handler).with(a: 2, b: 3, event_name: :handler)
       end
 
       it 'calls no method when there is no name match' do
         EventBus.subscribe(listener)
         EventBus.publish('b_method')
-        listener.should_not have_received(receiving_method)
+        listener.should_not have_received(:handler)
       end
 
       it 'will not accept other arguments' do
@@ -206,10 +205,10 @@ describe EventBus do
 
   describe '.clear' do
     it 'removes all previous registrants' do
-      EventBus.subscribe(event_name, listener, receiving_method)
+      EventBus.subscribe(event_name, listener, :handler)
       EventBus.clear
       EventBus.publish(event_name, {})
-      listener.should_not have_received(receiving_method)
+      listener.should_not have_received(:handler)
     end
 
     it 'returns itself, to facilitate cascades' do
